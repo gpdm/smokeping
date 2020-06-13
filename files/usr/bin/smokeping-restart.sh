9127b2a76f2e0f36eab2a6d9b7e0eafd820aef62
Gianpaolo Del Matto <delmatto@phunsites.net>
2020-06-13 20:05:04 +0000

    consider Smokeping CGI for restart as well

--- files/usr/bin/smokeping-restart.sh
+++ files/usr/bin/smokeping-restart.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
+
+# restart Smokeping daemon
+# NOTE: Smokeping actually supports the '--reload' command,
+#	however, that's not currently implenented in upstreams s6
 /bin/s6-svc -wr -t /var/run/s6/services/smokeping
+
+# kill the Smokeping CGI frontend as well
+/usr/bin/pkill -9 -f '/usr/bin/perl /usr/bin/smokeping_cgi /etc/smokeping/config'
